855b096c6f48f1f6eae1b51ba030321b79f34e45
User <rakeshgn97@gmail.com>
2018-01-14 22:47:38 +0530

    aboutpage

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 var About = React.createClass({
   render: function () {
     return (
+       <div>
       <h3>About Component</h3>
+      <p>Welcome to about page</p>
+      </div>
     )
   }
 });
