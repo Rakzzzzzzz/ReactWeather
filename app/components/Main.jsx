5daa5516a749a05b2a1402f76d9552e4429e405f
User <rakeshgn97@gmail.com>
2018-01-15 20:43:31 +0530

    main and example style changes

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,10 +5,13 @@ var Main = React.createClass({
   render: function () {
     return (
       <div>
-        <Nav/>
-        <h2>Main Component</h2>
-        {this.props.children}
+      <Nav/>
+      <div className="row">
+        <div className="columns medium-6 large-4 small-centered">
+          {this.props.children}
+        </div>
       </div>
+    </div>
     );
   }
 });
